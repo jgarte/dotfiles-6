4871617bc7a134791fb9967c94fe2d3d8ffb421e
Brad Parker <hi@bradparker.com>
2017-12-16 13:06:44 +1000

    Simplify path - remove edh/plain, remove coreutils

--- config/path.sh
+++ config/path.sh
@@ -1,4 +1,2 @@
-export PATH="/usr/local/bin:/usr/local/sbin:$PATH"
-export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
+export PATH="$HOME/.local/bin:$PATH"
 export PATH="$HOME/.dotfiles/bin:$PATH"
-export PATH="$HOME/code/plain-utils/bin:$PATH"
