3cebf494ed33914c5f8b03e7b647f69a70c57644
Brad Parker <hi@bradparker.com>
2018-01-05 14:43:25 +1000

    Add up and down arrow history nav

--- config/history.sh
+++ config/history.sh
@@ -8,3 +8,8 @@ shopt -s histappend
 # for setting history length see HISTSIZE and HISTFILESIZE in bash(1)
 HISTSIZE=1000
 HISTFILESIZE=2000
+
+bind '"\e[A":history-search-backward'
+bind '"\e[B":history-search-forward'
+bind '"\e[C": forward-char'
+bind '"\e[D": backward-char'
