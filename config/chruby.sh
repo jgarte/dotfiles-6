f556212500e88bdeca349a0862b9c5bc3a3f5d8c
Brad Parker <hi@bradparker.com>
2018-01-08 12:47:11 +1000

    Use newer ruby by default

--- config/chruby.sh
+++ config/chruby.sh
@@ -2,5 +2,5 @@ export CHRUBY_DIR="$HOME/.chruby"
 [ -s "$CHRUBY_DIR/share/chruby/chruby.sh" ] && . "$CHRUBY_DIR/share/chruby/chruby.sh"
 [ -s "$CHRUBY_DIR/share/chruby/auto.sh" ] && . "$CHRUBY_DIR/share/chruby/auto.sh"
 if [ -x "$(command -v chruby)" ]; then
-  chruby 2.3.1
+  chruby 2.4.1
 fi
