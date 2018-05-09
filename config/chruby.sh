8fb6d76c7f12fdae5db8d0cd6172eef2b8b013ed
Brad Parker <hi@bradparker.com>
2018-05-09 14:33:51 +1000

    Remove unused chruby config

--- config/chruby.sh
+++
@@ -1,6 +0,0 @@
-export CHRUBY_DIR="$HOME/.chruby"
-[ -s "$CHRUBY_DIR/share/chruby/chruby.sh" ] && . "$CHRUBY_DIR/share/chruby/chruby.sh"
-[ -s "$CHRUBY_DIR/share/chruby/auto.sh" ] && . "$CHRUBY_DIR/share/chruby/auto.sh"
-if [ -x "$(command -v chruby)" ]; then
-  chruby 2.4.1
-fi
