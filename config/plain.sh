cbf14a7c88b21829ba2d1bc68926a9e560bcfd69
Brad Parker <hi@bradparker.com>
2018-05-09 14:38:10 +1000

    Remove unused edh plain tool config

--- config/plain.sh
+++
@@ -1,9 +0,0 @@
-UTILS_DIR="$HOME/code/edh/plain-utils/bin"
-if [ -d "$UTILS_DIR" ]; then
-  export PATH="$UTILS_DIR:$PATH"
-fi
-
-SERVICES_DIR="$HOME/code/edh/plain-services-docker/bin"
-if [ -d "$SERVICES_DIR" ]; then
-  export PATH="$SERVICES_DIR:$PATH"
-fi
