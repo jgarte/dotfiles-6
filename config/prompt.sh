09b67e8c4ad60a27db525e56b3394c8409b33433
Brad Parker <hi@bradparker.com>
2018-06-28 12:07:53 +1000

    Remove unused color forcing stuff from PS1

--- config/prompt.sh
+++ config/prompt.sh
@@ -3,22 +3,6 @@ case "$TERM" in
   xterm-color|*-256color) color_prompt=yes;;
 esac
 
-# uncomment for a colored prompt, if the terminal has the capability; turned
-# off by default to not distract the user: the focus in a terminal window
-# should be on the output of commands, not on the prompt
-#force_color_prompt=yes
-
-if [ -n "$force_color_prompt" ]; then
-  if [ -x /usr/bin/tput ] && tput setaf 1 >&/dev/null; then
-	# We have color support; assume it's compliant with Ecma-48
-	# (ISO/IEC-6429). (Lack of such support is extremely rare, and such
-	# a case would tend to support setf rather than setaf.)
-    color_prompt=yes
-  else
-    color_prompt=
-  fi
-fi
-
 git_dirty () {
   local status=$(git status --porcelain 2> /dev/null)
   if [[ "$status" != "" ]]; then
@@ -50,4 +34,5 @@ if [ "$color_prompt" = yes ]; then
 else
   PS1="\u@\h\$(nix_shell_status)\$(git_branch)\$(git_dirty)\n\w » "
 fi
+
 unset color_prompt force_color_prompt
