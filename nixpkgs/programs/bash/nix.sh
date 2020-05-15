60fd6137ae54bd854913b163ffc7130a8a22c9f3
Brad Parker <hi@bradparker.com>
2020-05-15 17:50:21 +1000

    Better factor Nix Bash set up

--- nixpkgs/programs/bash/nix.sh
+++ nixpkgs/programs/bash/nix.sh
@@ -1,11 +1,20 @@
-nixpkgs="https://releases.nixos.org/nixos/unstable/nixos-20.03pre211190.a21c2fa3ea2/nixexprs.tar.xz"
-home_manager="https://github.com/rycee/home-manager/archive/f487b527ec420b888c52df1c4f8c31439201edb7.tar.gz"
+set_nix_path () {
+  local nixpkgs="https://releases.nixos.org/nixos/unstable/nixos-20.03pre211190.a21c2fa3ea2/nixexprs.tar.xz"
+  local home_manager="https://github.com/rycee/home-manager/archive/f487b527ec420b888c52df1c4f8c31439201edb7.tar.gz"
+  export NIX_PATH="nixpkgs=$nixpkgs:home-manager=$home_manager"
+}
 
-export NIX_PATH="nixpkgs=$nixpkgs:home-manager=$home_manager"
-export PATH="$HOME/.nix-profile/bin:$PATH"
+source_nix_env () {
+  local nix_profile_env="/nix/var/nix/profiles/default/etc/profile.d/nix.sh"
 
-profile_env="$HOME/.nix-profile/etc/profile.d/nix.sh"
+  if [ -f $nix_profile_env ]; then
+    . $nix_profile_env
+  fi
+}
 
-if [ -f $profile_env ]; then
-  . $profile_env
-fi
+init_nix () {
+  set_nix_path
+  source_nix_env
+}
+
+init_nix
