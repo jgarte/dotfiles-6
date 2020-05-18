bccf21e94880c816c16facbdf6b996e939bc4ed0
Brad Parker <hi@bradparker.com>
2020-05-18 10:33:09 +1000

    Make Nix shell script ZSH friendly

--- nixpkgs/programs/bash/nix.sh
+++ nixpkgs/programs/bash/nix.sh
@@ -1,7 +1,10 @@
 set_nix_path () {
   local nixpkgs="https://releases.nixos.org/nixos/unstable/nixos-20.03pre211190.a21c2fa3ea2/nixexprs.tar.xz"
+  local nixpkgs_path="nixpkgs=$nixpkgs"
   local home_manager="https://github.com/rycee/home-manager/archive/f487b527ec420b888c52df1c4f8c31439201edb7.tar.gz"
-  export NIX_PATH="nixpkgs=$nixpkgs:home-manager=$home_manager"
+  local home_manager_path="home-manager=$home_manager"
+
+  export NIX_PATH=$nixpkgs_path:$home_manager_path
 }
 
 source_nix_env () {
