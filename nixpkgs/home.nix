61a26765046cdf6e961d4bea0f499ebf2a178d36
Brad Parker <hi@bradparker.com>
2020-05-20 16:05:41 +1000

    Manage 4k fix in home-manager

--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -85,6 +85,38 @@ rec {
 
   powerline-fonts.enable = true;
 
+  programs.autorandr = {
+    enable = builtins.currentSystem != "x86_64-darwin";
+
+    profiles = {
+      "aoc" = {
+        fingerprint = {
+          HDMI-1 = "00ffffffffffff0005e390273f0801001e1d0103803c22782a67a1a5554da2270e5054bfef00d1c0b30095008180814081c001010101a36600a0f0701f803020350055502100001a565e00a0a0a029503020350055502100001e000000fc005532373930420a202020202020000000fd0017501e631e000a2020202020200188020326f14b101f05140413031202110123090707830100006d030c002000383c200060010203023a801871382d40582c450055502100001e011d007251d01e206e28550055502100001e8c0ad08a20e02d10103e96005550210000184d6c80a070703e8030203a0055502100001a000000000000000000000000000000000098";
+          eDP-1 = "00ffffffffffff00387000000000000000190104a51d117806de50a3544c99260f5054000000010101010101010101010101010101011a3680a070381f403020350026a510000018542b80a070381f403020350026a510000018000000100000000000000000000000000000000000fc004c433133334c46324c30310a200022";
+        };
+        config = {
+          DP-1.enable = false;
+          HDMI-2.enable = false;
+          DP-2.enable = false;
+          HDMI-1 = {
+            enable = true;
+            mode = "3456x1944_30.00";
+            position = "0x0";
+            primary = true;
+            rate = "29.97";
+          };
+          eDP-1 = {
+            enable = true;
+            mode = "1920x1080";
+            position = "3456x1347";
+            rate = "59.93";
+          };
+        };
+      };
+    };
+  };
+  home.file.".xsessionrc".text = builtins.readFile ./programs/x/xsessionrc;
+
   home.file.".gitignore".text = builtins.readFile ./programs/git/gitignore;
   home.file.".gitconfig".text = builtins.readFile ./programs/git/gitconfig;
 
@@ -95,8 +127,6 @@ rec {
   home.file.".local/share/git-completion.bash" = {
     source = "${builtins.fetchTarball "https://github.com/git/git/archive/master.tar.gz"}/contrib/completion/git-completion.bash";
   };
-
-  home.file.".config/termonad/termonad.hs".text = builtins.readFile ./programs/termonad/termonad.hs;
 
   home.file.".bashrc".text = ''
     ${builtins.readFile ./programs/bash/base-16.sh}
