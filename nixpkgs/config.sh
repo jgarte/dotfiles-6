a55362710e68a393b31154a884802b4a11a60ec6
Brad Parker <hi@bradparker.com>
2020-06-04 10:12:34 +1000

    Upgrade nixpkgs

--- nixpkgs/config.sh
+++ nixpkgs/config.sh
@@ -1,4 +1,4 @@
-export NIXPKGS="https://releases.nixos.org/nixos/unstable/nixos-20.03pre211190.a21c2fa3ea2/nixexprs.tar.xz"
+export NIXPKGS="https://releases.nixos.org/nixpkgs/nixpkgs-20.09pre228165.00df2371122/nixexprs.tar.xz"
 export HOME_MANAGER="https://github.com/rycee/home-manager/archive/f487b527ec420b888c52df1c4f8c31439201edb7.tar.gz"
 
 set_nix_path () {
