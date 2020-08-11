42f80ecbf25036657ee6ef97101cac9582df1203
Brad Parker <hi@bradparker.com>
2020-08-11 12:34:27 +1000

    Fix gifit for videos shorter than 1 minute
    What a silly mistake

--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -38,7 +38,6 @@ let
       #!${runtimeShell}
 
       ${ffmpeg}/bin/ffmpeg \
-        -ss 61.0 \
         -i $1 \
         -filter_complex "[0:v] fps=12,scale=720:-1,split [a][b];[a] palettegen [p];[b][p] paletteuse" \
         $2
